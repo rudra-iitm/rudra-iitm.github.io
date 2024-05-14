e241514c7363d29ca404f57ba72befdff9a73e2c
Rudra-IITM <singhrudra5556@gmail.com>
2024-05-15 02:20:11 +0530

    config vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
  
 export default defineConfig({
   plugins: [react()],
+  base: "/rudra-iitm.github.io/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
